6e7780088524aacf8c6e5246ed7a329e962e5247
Alessandro Tilli <alessandro.tilli@gmail.com>
2024-09-05 14:40:35 +0200

    test: 💍 1283*720 viewport

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -24,7 +24,7 @@ const config: TestRunnerConfig = {
     initDatabase();
   },
   async preVisit(page) {
-    page.setViewportSize({ width: 1282, height: 720 });
+    page.setViewportSize({ width: 1283, height: 720 });
 
     page
       .evaluate<SBStory>(() => {
